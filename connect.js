773979ed405598a9e811f14b7e148e25fa51931b
etnewell <ethannewell@me.com>
2021-02-28 14:28:31 -0800

    commented out conect.js and fixed teh heroku enviornment

--- connect.js
+++ connect.js
@@ -1,21 +1,21 @@
-const { MongoClient } = require("mongodb");
+// const { MongoClient } = require("mongodb");
 	 
-	// Replace the following with your Atlas connection string                                                                                                                                        
-	const url = "mongodb+srv://etnewell:IadD28F5lw0BkUWx@cluster0.vnsqu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-	const client = new MongoClient(url);
+// 	// Replace the following with your Atlas connection string                                                                                                                                        
+// 	const url = "mongodb+srv://etnewell:IadD28F5lw0BkUWx@cluster0.vnsqu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// 	const client = new MongoClient(url);
 
 
-async function run() {
-    	    try {
-    	        await client.connect();
-    	        console.log("Connected correctly to server");
+// async function run() {
+//     	    try {
+//     	        await client.connect();
+//     	        console.log("Connected correctly to server");
     	
-    	    } catch (err) {
-    	        console.log(err.stack);
-        }
-    	    finally {
-    	        await client.close();
-    	    }
-    	}
+//     	    } catch (err) {
+//     	        console.log(err.stack);
+//         }
+//     	    finally {
+//     	        await client.close();
+//     	    }
+//     	}
     	
-    	run().catch(console.dir);
+//     	run().catch(console.dir);
