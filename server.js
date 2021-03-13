7cd4cafa45b3f4e91a64fd1e50e137086d3a3eb2
etnewell <ethannewell@me.com>
2021-03-12 17:08:16 -0800

    here

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(express.static("models"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetManage", {
   useNewUrlParser: true,
